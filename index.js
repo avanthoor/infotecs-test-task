ba2e853ff940f127cd3c7104e6d5c9af1567e515
avanthoor <92603778+avanthoor@users.noreply.github.com>
2022-01-05 20:46:30 +0300

    added edit form

--- index.js
+++ index.js
@@ -30,12 +30,11 @@ for (let i = 0; i < date.length; i++) {
   columnLastName[i].textContent = date[i].name.lastName
   columnAbout[i].textContent = date[i].about
   columnEyeColor[i].textContent = date[i].eyeColor
-
-  rows[i].id = date[i].id
 }
 
 //Создаём и заполяем заголовочные ячейки
 const headerRow = document.createElement('tr')
+headerRow.id = 'headerRow'
 
 table.prepend(headerRow)
 for (let i = 0; i < 4; i++) {
@@ -50,7 +49,7 @@ headerCells[2].innerHTML = '<div>Описание <br>(about)</div>'
 headerCells[3].innerHTML = '<div>Цвет глаз (eyeColor)</div>'
 
 // Реализуем сортировку колокнок при нажатии на соответствующую кнопку
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < headerCells.length; i++) {
   const img = new Image(20, 20)
   img.src = 'https://img.icons8.com/material/50/000000/sorting-arrows--v1.png'
   headerCells[i].append(img)
@@ -76,7 +75,7 @@ const sortZA = (column) => {
 // от A до Z и от Z до A поочерёдно
 let clickCounter = 0
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < headerCells.length; i++) {
   imgs[i].addEventListener('click', () => {
     clickCounter++
     clickCounter % 2 === 0 ? sortZA(i) : sortAZ(i)
@@ -93,7 +92,7 @@ table.after(formContainer)
 const form = document.createElement('form')
 formContainer.append(form)
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < headerCells.length; i++) {
   form.append(document.createElement('h3'))
   form.append(document.createElement('input'))
 }
@@ -120,27 +119,45 @@ formContainer.style.visibility = 'hidden' // форма редактирован
 table.addEventListener('click', e => {
   let tr = e.target.closest('tr') // при клике на строки e.target === td, поэтому используем метод closest
 
-  if (tr) {
-    let id = tr.id // запоминаем id строки по которой кликнули
+  formContainer.style.visibility = 'visible' // делаем видимым блок редактирования
 
-    formContainer.style.visibility = 'visible'
-
-    tr.style.background = 'orange'
-
-    console.log(id)
-    
-    rows.forEach(el => {   // Получаем доступ к ячейкам кликнутой строки 
-      if (el.id === id) { 
-
-        formBtn.addEventListener('click', () => {
-          el.children[0].textContent = inputs[0].value
-        
-        })
-      }
-    })
+  for (let row of rows) {
+    row.removeAttribute('data-clicked') // после кажого клика удаляем все атрибуты у строк
+    row.style.background = '#fff' // и убираем заливку строки, на которую кликнули
   }
+
+  if (tr && tr.id !== 'headerRow') { // игнорируем заголовочную строку
+    tr.dataset.clicked = 'clicked' // если клик произошёл про строке - устанавливаем дата-атрибут 'clicked' кликнутой строке
+    
+    if (tr.hasAttribute('data-clicked')) {
+      tr.style.background = 'orange' // окрашиваем кликнутую строку
+    }
+  } 
+
 })
 
+formBtn.addEventListener('click', () => {
+  for (let row of rows) {
+    row.style.background = '#fff' // убираем заливку со строки
 
+    if (row.dataset.clicked) {
+      for (let i = 0; i < headerCells.length; i++) {
+        if (inputs[i].value !== '') {
+          row.children[i].textContent = inputs[i].value
+        }
+      }
+    }
+  }
+
+  for (let input of inputs) {  //очищаем input'ы после каждого редактирования
+    input.value = ''
+  }
+
+  formContainer.style.visibility = 'hidden' // скрываем блок редактирования после нажатия на клавишу "Редактировать"
+})
+
+/* for (let cell of columnAbout) {
+  console.log(cell.style.height)
+} */
 
 
