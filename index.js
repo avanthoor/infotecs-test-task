0d0a285609bbfacdc2344628b9ecb611aafba013
avanthoor <92603778+avanthoor@users.noreply.github.com>
2022-01-05 15:19:54 +0300

    sorting work

--- index.js
+++ index.js
@@ -1,55 +1,145 @@
-import date from './date.json' //Читаем date.json локально при помощи возможностей сборщика Parcel
+// Читаем date.json локально при помощи возможностей сборщика Parcel
+import date from './date.json'
 
-//Создаём таблицу
+// Создаём таблицу 
 const table = document.createElement('table')
  
 document.body.prepend(table)
 
-//Создаём колонки
-
-for (let i = 0; i < 4; i++) {
+for (let el of date) {
   table.append(document.createElement('tr'))
 }
 
-//и именуем их
+const rows = document.querySelectorAll('tr')
 
-const tr = document.querySelectorAll('tr')
-
-const name = tr[0]
-const lastName = tr[1]
-const about = tr[2]
-const eyeColor = tr[3]
-
-name.textContent = 'Имя'
-lastName.textContent = 'Фамилия'
-about.textContent = 'Описание'
-eyeColor.textContent = 'Цвет глаз'
-
-//Создаём ячейки в колонках
-
-for (let i = 0; i < date.length; i++) {
-  name.append(document.createElement('td'))
-  lastName.append(document.createElement('td'))
-  about.append(document.createElement('td'))
-  eyeColor.append(document.createElement('td'))
+for (let row of rows) {
+  for (let i = 0; i < 4; i++) {
+    row.append(document.createElement('td'))
+  }
 }
 
-//Выбираем все ячейки, принадлежащие своим колонкам
-const trName = document.querySelectorAll('tr:nth-child(1) > td') 
-const trLastName = document.querySelectorAll('tr:nth-child(2) > td') 
-const trAbout = document.querySelectorAll('tr:nth-child(3) > td') 
-const trEyeColor = document.querySelectorAll('tr:nth-child(4) > td') 
+// Заполняем все колонки таблицы соответствующими данными из JSON-файла
+const columnName = document.querySelectorAll('tr > td:nth-child(1)')
+const columnLastName = document.querySelectorAll('tr > td:nth-child(2)')
+const columnAbout = document.querySelectorAll('tr > td:nth-child(3)')
+const columnEyeColor = document.querySelectorAll('tr > td:nth-child(4)')
 
-//Заполняем табицу данными
+
 for (let i = 0; i < date.length; i++) {
-  trName[i].textContent = date[i].name.firstName
-  trLastName[i].textContent = date[i].name.lastName
-  trAbout[i].textContent = date[i].about
-  trEyeColor[i].textContent = date[i].eyeColor
+  columnName[i].textContent = date[i].name.firstName
+  columnLastName[i].textContent = date[i].name.lastName
+  columnAbout[i].textContent = date[i].about
+  columnEyeColor[i].textContent = date[i].eyeColor
+
+  rows[i].id = date[i].id
 }
 
+//Создаём и заполяем заголовочные ячейки
+const headerRow = document.createElement('tr')
 
+table.prepend(headerRow)
+for (let i = 0; i < 4; i++) {
+  headerRow.append(document.createElement('th'))
+}
 
+const headerCells = document.querySelectorAll('th')
+
+headerCells[0].innerHTML = '<div>Имя (firstName)</div>'
+headerCells[1].innerHTML = '<div>Фамилия (lastName)</div>'
+headerCells[2].innerHTML = '<div>Описание <br>(about)</div>'
+headerCells[3].innerHTML = '<div>Цвет глаз (eyeColor)</div>'
+
+// Реализуем сортировку колокнок при нажатии на соответствующую кнопку
+for (let i = 0; i < 4; i++) {
+  const img = new Image(20, 20)
+  img.src = 'https://img.icons8.com/material/50/000000/sorting-arrows--v1.png'
+  headerCells[i].append(img)
+}
+
+const imgs = document.querySelectorAll('img')
+
+// Функция сортировки
+const sortAZ = (column) => {
+  let sorted = Array.from(rows)
+    .sort((a, b) => a.cells[column].textContent > b.cells[column].textContent ? 1 : -1)
+
+  table.append(...sorted)
+}
+
+const sortZA = (column) => {
+  let sorted = Array.from(rows)
+    .sort((a, b) => a.cells[column].textContent > b.cells[column].textContent ? -1 : 1)
+
+  table.append(...sorted)
+}
+// Вешаем обработчики событий на соответствующие кнопки. Теперь при кликах на кнопки данные будут сортироваться
+// от A до Z и от Z до A поочерёдно
+let clickCounter = 0
+
+for (let i = 0; i < 4; i++) {
+  imgs[i].addEventListener('click', () => {
+    clickCounter++
+    clickCounter % 2 === 0 ? sortZA(i) : sortAZ(i)
+  })
+}
+
+// Редактирование данных
+// Создадим div с формой редактирования данных
+
+const formContainer = document.createElement('div')
+formContainer.id = 'formContainer'
+table.after(formContainer)
+
+const form = document.createElement('form')
+formContainer.append(form)
+
+for (let i = 0; i < 4; i++) {
+  form.append(document.createElement('h3'))
+  form.append(document.createElement('input'))
+}
+
+const inputHeaders = document.querySelectorAll('h3')
+const inputs = document.querySelectorAll('input')
+
+const formBtn = document.createElement('button')
+formContainer.append(formBtn)
+
+formBtn.textContent = 'Редактировать'
+
+// Озаглавим input'ы
+
+inputHeaders[0].textContent = 'Имя'
+inputHeaders[1].textContent = 'Фамилия'
+inputHeaders[2].textContent = 'Описание'
+inputHeaders[3].textContent = 'Цвет глаз'
+
+// Реализуем функцию редактирования данных при клике на соответствующую строку
+
+formContainer.style.visibility = 'hidden' // форма редактирования по умолчанию скрыта
+
+table.addEventListener('click', e => {
+  let tr = e.target.closest('tr') // при клике на строки e.target === td, поэтому используем метод closest
+
+  if (tr) {
+    let id = tr.id // запоминаем id строки по которой кликнули
+
+    formContainer.style.visibility = 'visible'
+
+    tr.style.background = 'orange'
+
+    console.log(id)
+    
+    rows.forEach(el => {   // Получаем доступ к ячейкам кликнутой строки 
+      if (el.id === id) { 
+
+        formBtn.addEventListener('click', () => {
+          el.children[0].textContent = inputs[0].value
+        
+        })
+      }
+    })
+  }
+})
 
 
 
